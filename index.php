9fb26024d611a614281e319b1f539a65c4d204fc
IqbalHazda <114972981+IqbalHazda@users.noreply.github.com>
2024-07-26 13:40:21 +0700

    Belajar Inheritance

--- index.php
+++ index.php
@@ -9,6 +9,17 @@ class Handphone {
 
 }
 
+class Vivo extends Handphone{
+
+}
+
+class Iphone extends Handphone{
+    function SmoothCamera(){
+        echo "Kamera Jernih";
+    }
+}
+
+
 $hp = new Handphone;
 $hp->merk = "Vivo";
 $hp->tipe = "V2020SE";
@@ -17,21 +28,14 @@ $hp->processor = "Snapdragon 1000";
 $hp->warna = "Hitam";
 $hp->harga = "Rp.2.000.000,00";
 
-$hp2 = new Handphone;
-$hp2->merk = "Vivo";
-$hp2->tipe = "V2020SE";
-$hp2->display = "20:9";
-$hp2->processor = "Snapdragon 1000";
-$hp2->warna = "Hitam";
-$hp2->harga = "Rp.2.000.000,00";
+$iphone = new Iphone;
+$iphone->merk = "iphone";
+$iphone->tipe = "iphone 15 Promaghh";
+$iphone->display = "2556 x 1179";
+$iphone->processor = "A16 Bionic";
+$iphone->warna = "Gold";
+$iphone->harga = "Rp.15.000.000,00";
 
-echo "$hp2->tipe";
-echo "<br>";
-echo "$hp->tipe";
+echo $iphone->Processor();
 
-if($hp === $hp2){
-    echo "sama";
-}
-else{
-    echo "tidak";
-}
+
