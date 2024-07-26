dde1f1f8fb6d07612282fbd09fdc8f579b2cea77
IqbalHazda <114972981+IqbalHazda@users.noreply.github.com>
2024-07-26 13:20:29 +0700

    Belajar Instance Object

--- index.php
+++ index.php
@@ -17,4 +17,21 @@ $hp->processor = "Snapdragon 1000";
 $hp->warna = "Hitam";
 $hp->harga = "Rp.2.000.000,00";
 
-echo "Tipe ".$hp->Processor();
+$hp2 = new Handphone;
+$hp2->merk = "Vivo";
+$hp2->tipe = "V2020SE";
+$hp2->display = "20:9";
+$hp2->processor = "Snapdragon 1000";
+$hp2->warna = "Hitam";
+$hp2->harga = "Rp.2.000.000,00";
+
+echo "$hp2->tipe";
+echo "<br>";
+echo "$hp->tipe";
+
+if($hp === $hp2){
+    echo "sama";
+}
+else{
+    echo "tidak";
+}
